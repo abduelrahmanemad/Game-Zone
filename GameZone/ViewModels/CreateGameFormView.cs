48122ec7c81a79c6bc1e2322c31f7b7db77d1d0b
Abdulrahman Emad <104274128+abduelrahmanemad@users.noreply.github.com>
2024-04-09 17:23:49 +0200

    see you tomorrow

---
+++ GameZone/ViewModels/CreateGameFormView.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.ViewModels
+{
+    public class CreateGameFormView
+    {
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+        [MaxLength(500)]
+        public IFormFile Cover { get; set; } = default!;
+        public int CategoryId { get; set; }
+        public List<int> SelectedDevicesIds { get; set; } = new List<int>();
+
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+        public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+    }
+}
